8bf0de20bbd26b07260d0d56eec88676ff30b0a0
Carlos Eduardo <132010811+Kadudesu@users.noreply.github.com>
2023-08-15 14:00:44 -0300

    Add files via upload

---
+++ components/Header/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../Header/header.css';
+
+export default function Header(){
+    return(
+        <header>Trabalhando com componentes no React</header>
+    );
+}
